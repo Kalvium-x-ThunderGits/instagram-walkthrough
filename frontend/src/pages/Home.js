c4d3428ba9331bc917f1af4b0fdcae0490c0cd75
Abhisek730 <abhi.ag.kas@gmail.com>
2024-07-24 07:01:18 +0530

    added final code for video-7

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Link } from "react-router-dom";
 import LargeNav from "../components/headers/LargeNav/LargeNav"
 import MobileNav from "../components/headers/MobileNav/MobileNav"
+import Feed from "../components/Feed/Feed";
+import TopNav from "../components/headers/TopNav/TopNav";
 
 export default function Home(){
     return(
@@ -11,9 +13,11 @@ export default function Home(){
                 <div className = "lg:w-[18%] sm:w-none md:w-none h-[100vh] pt-10 px-3 border-r border-r-gray-500 sticky top-0 left-0 lg:block sm:hidden md:block hidden"> 
                 <LargeNav></LargeNav>
                  </div>
-                 <div className = "w-full h-auto py-1 px-3 border-t border-t-[#1d1d1d] fixed bottom-0 left-0 lg:hidden md:hidden sm:block z-50"><MobileNav></MobileNav></div>
+                 <div className = "w-full h-auto py-1 px-3 border-t border-t-[#1d1d1d] fixed bottom-0 left-0 lg:hidden md:hidden sm:block z-50 bg-white"><MobileNav></MobileNav></div>
+                 <div className = "w-full h-auto py-1 px-3 border-b border-b-[#1d1d1d] fixed top-0 left-0 lg:hidden md:hidden sm:block z-50 bg-white"><TopNav></TopNav></div>
                 {/* bottom navbar  */}
                 {/* feeds sections  */}
+                <Feed></Feed>
             </div>
        
     </div>
