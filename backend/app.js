8413bcf308594058b415c4dd58e9675cc17c6481
Abhisek730 <abhi.ag.kas@gmail.com>
2024-07-28 04:07:27 +0530

    added final code for milestone-11

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const sequelize = require("./config/db");
-const userRoutes = require("./routes/userRoutes")
+const userRoutes = require("./routes/userRoutes");
+const postRoutes = require("./routes/postRoutes")
 const cors = require("cors")
 const authorizeUser = require("./middlewares/authorizeUser")
 
@@ -12,7 +13,8 @@ app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use("/api/users", userRoutes);
+app.use("/api/users", userRoutes);;
+app.use("/api/posts", postRoutes )
 
 
 // Test API
