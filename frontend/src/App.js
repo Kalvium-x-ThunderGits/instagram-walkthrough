e678a73d2408a041d4aaaf90e8ac1fddfb769641
Abhisek730 <abhi.ag.kas@gmail.com>
2024-08-27 02:20:00 +0530

    added final code for milestone-28

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,29 +4,31 @@ import Home from "./pages/Home";
 import SignIn from "./pages/SignIn";
 import Profile from "./pages/Profile";
 import AppLayout from "./pages/AppLayout";
-import { BrowserRouter, Routes, Route,Navigate } from "react-router-dom";
-import { AuthProvider,useAuth } from "./context/AuthContext";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import MyFollowingPost from "./pages/MyFollowingPost";
+import { AuthProvider, useAuth } from "./context/AuthContext";
 
 
-function RoutesComponent(){
-  const {isAuthenticated} = useAuth()
+function RoutesComponent() {
+  const { isAuthenticated } = useAuth()
   console.log(isAuthenticated);
-  return(
+  return (
     <BrowserRouter>
 
-    <div className="App">
-      <Routes>
+      <div className="App">
+        <Routes>
 
-        <Route path="/" element= {isAuthenticated ? <AppLayout /> : <Navigate to= "/signin"/>}>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/profile/:username" element={<Profile />}></Route>
-        </Route>
-        <Route path="/signup" element={isAuthenticated ?  <Navigate to= "/"/> : <SignUp />}></Route>
-        <Route path="/signin" element={isAuthenticated ?  <Navigate to= "/"/> : <SignIn />}></Route>
-      </Routes>
+          <Route path="/" element={isAuthenticated ? <AppLayout /> : <Navigate to="/signin" />}>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/profile/:username" element={<Profile />}></Route>
+            <Route path="/following" element={<MyFollowingPost />}></Route>
+          </Route>
+          <Route path="/signup" element={isAuthenticated ? <Navigate to="/" /> : <SignUp />}></Route>
+          <Route path="/signin" element={isAuthenticated ? <Navigate to="/" /> : <SignIn />}></Route>
+        </Routes>
 
-    </div>
-  </BrowserRouter>
+      </div>
+    </BrowserRouter>
   )
 }
 
@@ -35,9 +37,9 @@ function App() {
   console.log("I am app");
 
   return (
-  <AuthProvider>
-    <RoutesComponent></RoutesComponent>
-  </AuthProvider>
+    <AuthProvider>
+      <RoutesComponent></RoutesComponent>
+    </AuthProvider>
 
 
   );
