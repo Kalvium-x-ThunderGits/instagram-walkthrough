3a3f04b9285ea44360a7fcd1f4757a7d8c6b5ae1
Abhishek Agrawal <86093849+Abhisek730@users.noreply.github.com>
2024-09-06 03:09:17 +0530

    Update CompleteProfile.js

--- frontend/src/components/GoogleLogin/CompleteProfile.js
+++ frontend/src/components/GoogleLogin/CompleteProfile.js
@@ -23,7 +23,7 @@ export default function CompleteProfile() {
             const data = await res.json()
 
             if (res.ok) {
-                alert(data.message)
+                
                 navigate("/")
             }
         } catch (err) {
